a017972554dd50f4fc4e666a298f80b0580521c6
Kalaitzidis Alexandros <alexkalaitzidis2@gmail.com>
2020-08-06 23:01:13 +0300

    Seed database with default user and 50 trapezia

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,32 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // $this->call(UserSeeder::class);
-    }
+	/**
+	 * Seed the application's database.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		DB::table('users')->insert([
+			'name' => 'default',
+			'surname' => 'user',
+			'password' => Hash::make('asdasdasd'),
+			'email' => 'asd@asd.asd',
+			'type' => 'adminos'
+		]);
+
+		for ($i = 0; $i < 50; $i++){
+			DB::table('trapezi')->insert([
+				'meros' => 'Paralia',
+				'customers' => mt_rand(2, 6)
+			]);
+		}
+	}
 }
