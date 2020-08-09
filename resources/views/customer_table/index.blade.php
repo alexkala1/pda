4575922f45944c619ce999860ed7c7bb2df562c0
Kalaitzidis Alexandros <alexkalaitzidis2@gmail.com>
2020-08-09 16:36:12 +0300

    Conditional show customer tables wether customer ordered or not

--- resources/views/customer_table/index.blade.php
+++ resources/views/customer_table/index.blade.php
@@ -4,7 +4,26 @@
 
 <div class="container-fluid">
 	<div class="row justify-content-start">
-		@foreach($customerTables as $customerTable) <div class="col-sm-4 col-md-3 col-lg-3 col-xl-2 col-6">
+		@foreach($customerTables as $customerTable)
+		@if ($customerTable->status == 'Ordered')
+		 <div class="col-sm-4 col-md-3 col-lg-3 col-xl-2 col-6">
+			<div class="card my-2">
+				<div class="card-header">
+					customerTable {{ $customerTable->id }}
+				</div>
+				<div class="card-body">
+					<div class="text-center">
+						Order status: {{ $customerTable->status }}
+						<br>
+						Customers: {{ $customerTable->customers}}
+					</div>
+					<br>
+					<a href="{{ route('customer_table.show' , $customerTable->id) }}" type="button" class="btn btn-block btn-primary">Submit an order</a>
+				</div>
+			</div>
+		</div>
+		@else
+		<div class="col-sm-4 col-md-3 col-lg-3 col-xl-2 col-6">
 			<div class="card my-2">
 				<div class="card-header">
 					customerTable {{ $customerTable->id }}
@@ -17,7 +36,8 @@
 					<a href="{{ route('customer_table.show' , $customerTable->id) }}" type="button" class="btn btn-block btn-primary">Submit an order</a>
 				</div>
 			</div>
-	</div>
+		</div>
+		@endif
 @endforeach
 
 </div>
